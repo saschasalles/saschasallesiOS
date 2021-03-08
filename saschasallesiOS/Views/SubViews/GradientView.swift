aaab4ec38d7ff284b7a79ee656ebf997f7aecc2d
Sascha Sallès <sascha.salles@ynov.com>
2021-03-08 14:07:49 +0100

    Add Gradient View

---
+++ saschasallesiOS/Views/SubViews/GradientView.swift
@@ -0,0 +1,31 @@
+//
+//  GradientView.swift
+//  saschasallesiOS
+//
+//  Created by Sascha Sallès on 08/03/2021.
+//
+
+import SwiftUI
+
+struct GradientView<Content>: View where Content: View {
+
+    let content: () -> Content
+
+    var body: some View {
+        VStack(alignment: .center) {
+            content()
+        }
+        .frame(minWidth: 320,
+               maxWidth: .infinity,
+               alignment: .topLeading)
+        .background(
+            LinearGradient(gradient:
+                Gradient(colors: [Color(.systemBackground), Color(.systemGray6)]), startPoint: .top, endPoint: .bottom))
+        .animation(.easeOut(duration: 0.15))
+    }
+
+    // note the optional parameter here
+    init(@ViewBuilder _ content: @escaping () -> Content) {
+        self.content = content
+    }
+}
